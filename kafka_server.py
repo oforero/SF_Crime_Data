8e628722e0cb60f09c746c2ec96a7d987fcf6e60
Oscar Mauricio Forero Carrillo <oforero@ieee.org>
2020-05-20 11:12:46 +0800

    feat: Step 1: implement producers

--- kafka_server.py
+++ kafka_server.py
@@ -1,16 +1,15 @@
 import producer_server
 
-
+BROKER_URL = "localhost:9092"
 def run_kafka_server():
-	# TODO get the json file path
-    input_file = ""
+    input_file = "police-department-calls-for-service.json"
 
-    # TODO fill in blanks
+    # Create a new produder
     producer = producer_server.ProducerServer(
         input_file=input_file,
-        topic="",
-        bootstrap_servers="",
-        client_id=""
+        topic="sf_crime_data.police.service.calls",
+        bootstrap_servers=BROKER_URL,
+        client_id="sf_data_crime.service.call.producer"
     )
 
     return producer
